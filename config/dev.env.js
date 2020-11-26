15611dec142567a0f6abe0012157cf7d18832821
Peng Jun Hong <office1289366@gmail.com>
2020-11-26 13:13:35 +0800

    API_URL setting

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_URL: '"http://ec2-18-191-37-62.us-east-2.compute.amazonaws.com"'
 })
